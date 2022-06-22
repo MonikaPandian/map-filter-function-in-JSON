f98573223df4136447e42cd977e56e16c8dc99ab
MonikaPandian <87970544+MonikaPandian@users.noreply.github.com>
2022-06-22 09:11:09 +0530

    mapfilter.html

---
+++ mapfilter2.js
@@ -0,0 +1,15 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+
+var cpopu = result.filter((ele)=>ele.population < 500000)
+    console.log(cpopu);
+
+
+   var final = cpopu.map((ele)=>ele.name);
+   console.log(final);
+}
